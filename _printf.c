086a7277f381c95468d9ab9e144594652e86fe6c
Dubale-setto <82271953+Dubale-setto@users.noreply.github.com>
2023-08-22 20:16:10 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,9 +9,8 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-
         int i, printed = 0, printed_chars = 0;
-
+        
         int flags, width, precision, size, buff_ind = 0;
 
         va_list list;
@@ -84,26 +83,18 @@ int _printf(const char *format, ...)
 
 }
 
-
 /**
-
  * print_buffer - Prints the contents of the buffer if it exist
-
  * @buffer: Array of chars
-
  * @buff_ind: Index at which to add next char, represents the length.
-
  */
 
 void print_buffer(char buffer[], int *buff_ind)
-
 {
-
         if (*buff_ind > 0)
 
                 write(1, &buffer[0], *buff_ind);
 
 
         *buff_ind = 0;
-
 }
