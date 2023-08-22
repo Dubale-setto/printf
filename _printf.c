de759a104196e7dd5f75de58af26478efe50f715
Dubale-setto <82271953+Dubale-setto@users.noreply.github.com>
2023-08-22 20:23:07 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -55,32 +55,26 @@ int _printf(const char *format, ...)
                         flags = get_flags(format, &i);
 
                         width = get_width(format, &i, list);
-
                         precision = get_precision(format, &i, list);
-
                         size = get_size(format, &i);
-
                         ++i;
-
+                        
                         printed = handle_print(format, &i, list, buffer,
-
                                 flags, width, precision, size);
-
+                        
                         if (printed == -1)
 
                                 return (-1);
+                        
 
                         printed_chars += printed;
                 }
         }
         print_buffer(buffer, &buff_ind);
-
-
+        
         va_end(list);
-
-
         return (printed_chars);
-
+        
 }
 
 /**
